fd2b355d80e7028f0ca7601d6523ae06b825a17a
jmore <jmorenorios5@gmail.com>
2024-09-25 23:12:12 +0200

    cambios vista detalles

--- src/views/Detalle.jsx
+++ src/views/Detalle.jsx
@@ -6,15 +6,18 @@ const Detalle = () => {
   const {id}  = useParams();
   const [Jugador, setJugador] = useState([]);
   const [valor, setValor] = useState([]);
-  const [estado, setEstado] = ([]);
-  const [puntosPromedio, setPuntosPromedio] = ([])
+  const [titularidad, setTitularidad] = useState(0);
+  const [puntosPromedio, setPuntosPromedio] = useState([]);
+  const [puntosTotales, setPuntosTotales] = useState([]);
   const [imagen,setImagen] = useState([]);
   const [escudo, setEscudo] = useState('');
 
   useEffect( () => {
-    getJugador()
+    getJugador();
+   
 }, [])
 
+console.log(Jugador)
 
 const getJugador = async() =>{
   const liga = 'https://api-fantasy.llt-services.com/api/v3/player/'+id;
@@ -29,18 +32,23 @@ const getJugador = async() =>{
 
     setEscudo(respuesta.team.badgeColor)
     setValor(respuesta.marketValue.toLocaleString('es-ES'))
+    setPuntosPromedio(respuesta.averagePoints.toString().substring(0, 5))
   })
+
+
 }
+
 
   return (
   <div className='contenedor'>
     <div class="contenedorJugador">
       <div className='circulo'>
         <img className='caraJugador' src={imagen}></img>
-        <h1 className='nombre'>{Jugador.nickname}</h1>
       </div>
       <div>
+        <h1 className='nombre'>{Jugador.nickname}</h1>
         <h4>Valor: {valor}</h4>
+        <h5>Puntos/Partido:  {puntosPromedio}</h5>
       </div>
     </div>
   </div>
